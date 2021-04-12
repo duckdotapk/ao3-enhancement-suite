165397aa66a572da007795cfa7e774f31550e355
Dr. Off <70072513+dr-off@users.noreply.github.com>
2021-04-12 19:24:22 -0400

    Partial localisation support.

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,8 @@
-new Setting("enable_search_presets", 					"Search Presets",	"Enable", 								"checkbox", 	true);
+new Setting("enable_search_presets", 					browser.i18n.getMessage("search_presets"),	browser.i18n.getMessage("enable"), 						"checkbox", 	true);
 
-new Setting("enable_tag_collapse",						"Tag Collapse",		"Enable",								"checkbox",		true);
-new Setting("tag_collapse_threshold",					"Tag Collapse",		"Threshold",							"number",		20,		{ min: 0 });
+new Setting("enable_tag_collapse",						browser.i18n.getMessage("tag_collapse"),	browser.i18n.getMessage("enable"),						"checkbox",		true);
+new Setting("tag_collapse_threshold",					browser.i18n.getMessage("tag_collapse"),	browser.i18n.getMessage("threshold"),					"number",		20,		{ min: 0 });
 
-new Setting("enable_average_words_per_chapter_stat", 	"Work Stats",		`Enable "Average words per chapter"`,	"checkbox",		true);
-new Setting("enable_estimated_reading_time_stat",		"Work Stats",		`Enable "Estimated reading time"`,		"checkbox",		true);
-new Setting("read_speed",								"Work Stats",		`Reading speed (wpm)`,						"number",		200,	{ min: 1 });
+new Setting("enable_average_words_per_chapter_stat", 	browser.i18n.getMessage("work_stats"),		browser.i18n.getMessage("enable_titled", [ browser.i18n.getMessage("average_words_per_chapter") ]),		"checkbox",		true);
+new Setting("enable_estimated_reading_time_stat",		browser.i18n.getMessage("work_stats"),		browser.i18n.getMessage("enable_titled", [ browser.i18n.getMessage("estimated_reading_time") ]),		"checkbox",		true);
+new Setting("read_speed",								browser.i18n.getMessage("work_stats"),		browser.i18n.getMessage("reading_speed"),																"number",		200,	{ min: 1 });
