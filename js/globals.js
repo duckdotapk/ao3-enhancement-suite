ecacc040a002b6f3b73c74a585407e08716e13e5
Dr. Off <70072513+dr-off@users.noreply.github.com>
2021-04-18 00:46:29 -0400

    Adding the current version to the About Modal.

--- js/globals.js
+++ js/globals.js
@@ -16,6 +16,13 @@ aesDropdown.addItem(browser.i18n.getMessage("about"), function(event, item)
 	let header = document.createElement("h1");
 	header.innerText = browser.i18n.getMessage("name");
 
+	{
+		let version = document.createElement("span");
+		version.classList.add("aes-small");
+		version.innerText = " v" + browser.runtime.getManifest().version;
+		header.appendChild(version);
+	}
+
 	let paragraph = document.createElement("p");
 	paragraph.innerText = browser.i18n.getMessage("description");
 
