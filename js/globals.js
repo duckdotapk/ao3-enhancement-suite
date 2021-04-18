4be0a0e70d0c4ca064556db9f7d1a31860fb9326
Dr. Off <70072513+dr-off@users.noreply.github.com>
2021-04-18 17:40:09 -0400

    Moving nav-dropdown stuff to a subfolder.

--- js/globals.js
+++ js/globals.js
@@ -10,25 +10,3 @@ const tagLists =
 const tagListsStr = tagLists.join(", ");
 
 const aesDropdown = new NavDropdown(browser.i18n.getMessage("name"));
-
-aesDropdown.addItem("about", browser.i18n.getMessage("about"), function(event, item)
-{
-	let header = document.createElement("h1");
-	header.innerText = browser.i18n.getMessage("name");
-
-	{
-		let version = document.createElement("span");
-		version.classList.add("aes-small");
-		version.innerText = " v" + browser.runtime.getManifest().version;
-		header.appendChild(version);
-	}
-
-	let paragraph = document.createElement("p");
-	paragraph.innerText = browser.i18n.getMessage("description");
-
-	Modal.show(
-	[
-		header,
-		paragraph,
-	]);
-});
