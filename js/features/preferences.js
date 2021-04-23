1940d829c173fa9e4b37d8bd6da1072842f6a759
Dr. Off <70072513+dr-off@users.noreply.github.com>
2021-04-23 13:24:07 -0400

    Reworking preferences.js.

--- js/features/preferences.js
+++ js/features/preferences.js
@@ -1,26 +1,28 @@
-(async function()
 {
-	let acronym = browser.i18n.getMessage("name_acronym");
-
+	new Feature("preferences", async function(setting)
 	{
-		let message = browser.i18n.getMessage("ao3_hide_setting_note", [ browser.i18n.getMessage("name_acronym"), browser.i18n.getMessage("archive_warnings_lc") ]);
+		const acronym = browser.i18n.getMessage("name_acronym");
 
-		let helpButton = new HelpButton(acronym, message, function(event)
 		{
-			Modal.alert(message);
-		});
-
-		document.querySelector(`label[for="preference_hide_warnings"]`).parentElement.appendChild(helpButton.element);
-	}
-
-	{
-		let message = browser.i18n.getMessage("ao3_hide_setting_note", [ browser.i18n.getMessage("name_acronym"), browser.i18n.getMessage("additional_tags_lc") ]);
-
-		let helpButton = new HelpButton(acronym, message, function(event)
+			const message = browser.i18n.getMessage("ao3_hide_setting_note", [ browser.i18n.getMessage("name_acronym"), browser.i18n.getMessage("archive_warnings_lc") ]);
+	
+			const helpButton = new HelpButton(acronym, message, function(event)
+			{
+				Modal.alert(message);
+			});
+	
+			document.querySelector(`label[for="preference_hide_warnings"]`).parentElement.appendChild(helpButton.element);
+		}
+	
 		{
-			Modal.alert(message);
-		});
-
-		document.querySelector(`label[for="preference_hide_freeform"]`).parentElement.appendChild(helpButton.element);
-	}
-})();
+			const message = browser.i18n.getMessage("ao3_hide_setting_note", [ browser.i18n.getMessage("name_acronym"), browser.i18n.getMessage("additional_tags_lc") ]);
+	
+			const helpButton = new HelpButton(acronym, message, function(event)
+			{
+				Modal.alert(message);
+			});
+	
+			document.querySelector(`label[for="preference_hide_freeform"]`).parentElement.appendChild(helpButton.element);
+		}
+	});
+}
